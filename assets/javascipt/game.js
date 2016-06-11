9d53642306b21c507da6054749d8ffa8e385e0b2
David Li <jdavidli01@outlook.com>
2016-06-11 19:42:00 -0400

    javascript changes

---
+++ assets/javascipt/game.js
@@ -0,0 +1,10 @@
+var wins = 0;//win
+var loses = 0;//lose
+
+var set;//number
+var total;//scoreBox
+
+
+var i = Math.floor(Math.random()*10)+1;
+console.log(i);
+//document.querySelector('.ruby').data(number: i);
---
+++ game.js
@@ -0,0 +1,11 @@
+var wins = 0;//win
+var loses = 0;//lose
+
+var set;//number
+var total;//scoreBox
+
+
+var i = Math.floor(Math.random()*10)+1;
+console.log(i);
+var el = document.querySelector(".ruby").setAttribute('data', "i");
+console.log(el);
