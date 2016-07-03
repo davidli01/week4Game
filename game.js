7917b55100c39d1254d996c005f3b9136c288ea7
David Li <jdavidli01@outlook.com>
2016-07-03 10:04:07 -0400

    assigned gems with .value

--- game.js
+++ game.js
@@ -4,51 +4,16 @@ var loses = 0;//lose
 var set;//number
 var total = 0;//scoreBox
 
+var ruby = document.querySelector(".ruby").value = Math.floor(Math.random()*10)+1;
+var emerald = document.querySelector(".emerald").value = Math.floor(Math.random()*10)+1;
+var topaz = document.querySelector(".topaz").value = Math.floor(Math.random()*10)+1;
+var sapphire = document.querySelector(".sapphire").value = Math.floor(Math.random()*10)+1;
 
-var elem1 = document.createElement("img");
-//set attributes for img tag
-elem1.setAttribute("class", "ruby");
-elem1.setAttribute("src", "assets/image/ruby.jpg");
-elem1.setAttribute("data-num", Math.floor(Math.random()*10)+1);
-//append this new elem1 on to box1 
-document.querySelector(".box1").appendChild(elem1);
+console.log(ruby);
 
-var elem2 = document.createElement("img");
-elem2.setAttribute("class", "sapphire");
-elem2.setAttribute("src", "assets/image/Sapphire.png");
-elem2.setAttribute("data-num", Math.floor(Math.random()*10)+1);
-document.querySelector(".box2").appendChild(elem2);
+var rubyNum = Math.floor(Math.random()*10)+1;
+console.log(rubyNum);
 
-var elem3 = document.createElement("img");
-elem3.setAttribute("class", "emerald");
-elem3.setAttribute("src", "assets/image/emerald.png");
-elem3.setAttribute("data-num", Math.floor(Math.random()*10)+1);
-document.querySelector(".box3").appendChild(elem3);
-
-var elem4 = document.createElement("img");
-elem4.setAttribute("class", "topaz");
-elem4.setAttribute("src", "assets/image/topaz.jpg");
-elem4.setAttribute("data-num", Math.floor(Math.random()*10)+1);
-document.querySelector(".box4").appendChild(elem4);
-
-/*
-var rubyNum = Math.floor(Math.random()*10)+1
-var sapphireNum = Math.floor(Math.random()*10)+1
-var emeraldNum = Math.floor(Math.random()*10)+1
-var topazNum = Math.floor(Math.random()*10)+1
-
-
-//assign the randon numbers above to the images (class)
-var el1 =document.querySelector(".ruby").innerHTML = rubyNum;
-var el2 =document.querySelector(".sapphire").innerHTML = sapphireNum;
-var el3 =document.querySelector(".emerald").innerHTML = emeraldNum;
-var el4 =document.querySelector(".topaz").innerHTML = topazNum;
-
-console.log(el1);
-console.log(el2);
-console.log(el3);
-console.log(el4);
-*/
 //assign random number for set number
 function randomNum() {
 	set = Math.floor(Math.random()*20)+30;
@@ -83,6 +48,8 @@ console.log(number);
 console.log(total);
 */
 
+
+/*
 //get attributes for gem elements (data-num)
 var gem = document.querySelectorAll('img');
 console.log(gem);
@@ -97,4 +64,4 @@ function addnum() {
 }
 document.querySelectorAll("img").onclick = addNum;
 
-
+*/
